559fca0521bfd13f32b9a5c2df20614a2d7d452a
jobayer sozib <zeni.sozibndc@gmail.com>
2016-04-03 06:32:05 +0000

    second commit

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@
  */
 
 var bps_obj={
+   
     create_node:function(e){
         
-         if(e.target.parentNode.nodeName==="svg"){
+         if( e.target.nodeName==='LINE' || e.target.parentNode.nodeName==="svg" ){
+                alert(e.target.nodeName);
                  e.target.parentNode.removeChild(e.target);
                 }
             else{
@@ -29,5 +31,32 @@ var bps_obj={
 }
     
     
-document.getElementById("mysvg").addEventListener("click",bps_obj.create_node,true);
+document.getElementById("mysvg").addEventListener("click",bps_obj.create_node,false);
 
+document.getElementById("submit").addEventListener("click",function(e) {
+     /*var svg=document.getElementById("mysvg");
+      var x1=document.getElementById("SX").value;
+      var y1=document.getElementById("SY").value;
+      var x2=document.getElementById("EX").value;
+      var y2=document.getElementById("EY").value;
+                   
+     svg.innerHTML +='<line x1='+x1+' y1='+y1+' x2='+x2+' y2='+y2+' stroke="black" stroke-width="3"/>';
+     
+     */
+     var svg=document.getElementById("mysvg");
+                    var svgNS = svg.namespaceURI;
+                    var el=document.createElementNS(svgNS,'line');
+                    var x1=document.getElementById("SX").value;
+                    var y1=document.getElementById("SY").value;
+                    var x2=document.getElementById("EX").value;
+                    var y2=document.getElementById("EY").value;
+                    el.setAttribute("x1",x1);
+                    el.setAttribute("y1",y1);
+                    el.setAttribute("x2",x2);
+                   
+                    el.setAttribute("y2",y2);
+                     el.setAttribute("stroke","black");
+                    el.setAttribute("stroke-width",3);
+                    svg.appendChild(el);
+     
+})
