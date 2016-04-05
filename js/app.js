f00c4f69d7a9a41895625b937eccd3325ca191e4
jobayer sozib <zeni.sozibndc@gmail.com>
2016-04-05 05:19:55 +0000

    node connect and map content

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
+/*
+
+object:bps_obj
+Drawing object . this object holds all drawing methods and poperties.
+
+*/
 
 var bps_obj={
+    /**
+     function:create_node
+     param:e(object)
+     this function will call if click event occur inside canvas area. this function will create circle and remove 
+     both line and circle in canvas area.
+    */
    
     create_node:function(e){
         
@@ -25,30 +34,21 @@ var bps_obj={
                     el.setAttribute("fill","red");
                     svg.appendChild(el);
                 }
-    }
+    },
     
-   
-}
-    
-    
-document.getElementById("mysvg").addEventListener("click",bps_obj.create_node,false);
-
-document.getElementById("submit").addEventListener("click",function(e) {
-     /*var svg=document.getElementById("mysvg");
-      var x1=document.getElementById("SX").value;
-      var y1=document.getElementById("SY").value;
-      var x2=document.getElementById("EX").value;
-      var y2=document.getElementById("EY").value;
-                   
-     svg.innerHTML +='<line x1='+x1+' y1='+y1+' x2='+x2+' y2='+y2+' stroke="black" stroke-width="3"/>';
-     
-     */
-     var svg=document.getElementById("mysvg");
+    /**
+     function:create_line
+     param:e(object)
+     this function will call if click event occur inside canvas area. this function will create line.
+    */
+    create_line:function(e){
+                    
+                    var svg=document.getElementById("mysvg");
                     var svgNS = svg.namespaceURI;
                     var el=document.createElementNS(svgNS,'line');
-                    var x1=document.getElementById("SX").value;
+                    var x1=Number(document.getElementById("SX").value)+10;
                     var y1=document.getElementById("SY").value;
-                    var x2=document.getElementById("EX").value;
+                    var x2=Number(document.getElementById("EX").value)-10;
                     var y2=document.getElementById("EY").value;
                     el.setAttribute("x1",x1);
                     el.setAttribute("y1",y1);
@@ -59,4 +59,31 @@ document.getElementById("submit").addEventListener("click",function(e) {
                     el.setAttribute("stroke-width",3);
                     svg.appendChild(el);
      
-})
+
+    }
+    
+   
+}
+    
+    
+document.getElementById("mysvg").addEventListener("click",bps_obj.create_node,false);
+
+document.getElementById("submit").addEventListener("click",bps_obj.create_line,false);
+
+
+
+
+addEventListener("mousemove",function(e){
+   if(e.target.nodeName==="circle"){
+       var x=e.target.getAttribute("cx");
+       var y=e.target.getAttribute("cy");
+    document.getElementById("mx").innerHTML="Mouse X position: "+x;
+    document.getElementById("my").innerHTML="Mouse Y position: "+y;
+   }
+},false);
+
+
+addEventListener("load",setInterval(function(){
+    document.getElementById("maptext").textContent=document.getElementById("mysvg").innerHTML;
+    
+},1000));
